a09c8434476a8e98bd97f258a801757e00aafcce
Victor Hugo <victorferrarihugo@hotmail.com>
2025-04-14 20:56:02 -0300

    feat: Implements add new task method

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:my_app/views/screens/home.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowMaterialGrid: false,
+      debugShowCheckedModeBanner: false,
       title: "TODO List App",
       home: HomePage(),
     ),
--- todo_app/lib/views/screens/home.dart
+++ todo_app/lib/views/screens/home.dart
@@ -14,6 +14,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final items = ToDo.toDoList();
 
+  final _todoController = TextEditingController();
+
   void _handleToDoState(ToDo item) {
     setState(() {
       item.isDone = !item.isDone;
@@ -26,40 +28,106 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _addNewTask(String toDo) {
+    setState(() {
+      items.add(
+        ToDo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          description: toDo,
+        ),
+      );
+    });
+    _todoController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 229, 225, 236),
       appBar: HomeAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: Text(
-                      "All ToDo Tasks",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          "All ToDo Tasks",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      for (ToDo item in items)
+                        ToDoItem(
+                          item: item,
+                          onToDoState: _handleToDoState,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    margin: EdgeInsets.only(bottom: 15, right: 10, left: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: InputDecoration(
+                        hintText: "Add a new task here",
+                        border: InputBorder.none,
                       ),
                     ),
                   ),
-                  for (ToDo item in items)
-                    ToDoItem(
-                      item: item,
-                      onToDoState: _handleToDoState,
-                      onDeleteItem: _deleteToDoItem,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 15, right: 10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      _addNewTask(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      minimumSize: Size(45, 45),
+                      elevation: 10,
                     ),
-                ],
-              ),
+                    child: Text(
+                      "+",
+                      style: TextStyle(fontSize: 23, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
